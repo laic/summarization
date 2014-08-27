aedfe6edc3c7cfe9f51ba55f98765abbc09779ce
Catherine Lai <clai@inf.ed.ac.uk>
2014-08-27 11:01:22 +0100

    batch script

--- ineventwrappers/inevent-batch.sh
+++ ineventwrappers/inevent-batch.sh
@@ -3,7 +3,7 @@
 PREFIX=$1
 for file in `ls ~/data/inevent/UEDIN_ASR_201407/$PREFIX*`
 do 
-	fstem=`basename $file`
-	echo  $fstem
-	./inevent-proc.sh $fstem
+	conv=`basename $file .json`
+	echo  $conv
+	./inevent-proc.sh $conv
 done
