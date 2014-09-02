e4150961bc9c55e6099d2b3a65a27311783b9595
Catherine Lai <clai@koyama.inf.ed.ac.uk>
2014-09-02 14:27:43 +0100

    sentence break experiments

--- sentence_breaks/apply-c5-tree.r
+++ sentence_breaks/apply-c5-tree.r
@@ -1,5 +1,48 @@
 library(C50)
-library(data.table)
+source("../rscripts/")
+library(openNLP)
+
+apply.punctree <- function(treefile, testfile) {
+	x <- load(treefile)
+	punctree <- get(x)
+	test.pos <- data.table(read.csv(testfile, header=F))
+
+	punctreePred <- predict(punctree, test.pos)
+	punctreeProbs <- data.table(test.pos, predict(punctree, test.pos, type ="prob"))
+	save(punctreeProbs, file="punctreeProbs")
+}
+
+demo.tokenizer <- function() {
+	s <- paste(c("Pierre Vinken, 61 years old, will join the board as a ",
+	"nonexecutive director Nov. 29.\n", "Mr. Vinken is chairman of Elsevier N.V.,
+	", "the Dutch publishing group."), collapse = "")
+
+
+	s <- as.String(s)
+	sent_token_annotator <- Maxent_Sent_Token_Annotator()
+	word_token_annotator <- Maxent_Word_Token_Annotator()
+	a2 <- annotate(s, list(sent_token_annotator, word_token_annotator))
+	pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+	a3 <- annotate(s, pos_tag_annotator, a2)
+	a3
+
+}
+
+tokenize.words <- function(currwords) {
+	s <- paste(tolower(currwords), collapse= " ")
+	s <- as.String(s)
+	sent_token_annotator <- Maxent_Sent_Token_Annotator()
+	word_token_annotator <- Maxent_Word_Token_Annotator()
+	tok.words <- annotate(s, list(sent_token_annotator, word_token_annotator))
+	pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+	pos.words <- annotate(s, pos_tag_annotator, tok.words)
+	
+
+	pw <- subset(pos.words, type == "word")
+	tags <- sapply(pw$features, `[[`, "POS")
+
+	tags
+}
 
 ################################################
 args=(commandArgs(TRUE))
@@ -9,17 +52,10 @@ if(length(args)==0){
 }
 ################################################
 
-treefile<- args[1]
-testfile<- args[2]
+#treefile<- args[1]
+wordfile<- args[1]
 
-x <- load("treefile")
-punctree <- get(x)
-test.pos <- data.table(read.csv(testfile, header=F))
 
-punctreePred <- predict(punctree, test.pos)
-punctreeProbs <- data.table(test.pos, predict(punctree, test.pos, type ="prob"))
-
-save(punctreeProbs, file="punctreeProbs")
 
 
 
