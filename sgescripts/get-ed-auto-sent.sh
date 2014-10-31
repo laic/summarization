bd4b7e011a220f07a6b16d83ff2520b7e846bf2e
Catherine Lai <clai@inf.ed.ac.uk>
2014-10-31 17:24:47 +0000

    auto sentencing wrapper

---
+++ sgescripts/get-ed-auto-sent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#$ -N write-auto-sent
+#$ -P inf_hcrc_cstr_inevent
+#$ -cwd
+#$ -o ./edout
+#$ -e ./edout
+#$ -l h_rt=06:00:00
+# Initialise environment module
+
+. /etc/profile.d/modules.sh
+
+export PATH=~/local/bin:$PATH
+
+# Run the program
+# -pe memory-2G 2
+
+filename=$1
+datadir=$2
+
+Rscript ../sentence_breaks/write-auto-sentences.r $filename $datadir
+
+
